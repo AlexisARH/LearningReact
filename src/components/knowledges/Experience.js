2b17afb68dbad3ebcce1abd1109a2504e059befb
Alexis__GG <alexisgregoire2899@gmail.com>
2021-03-12 14:12:36 +0100

    First commit

---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Experience = () => {
+	return (
+		<div className="experience">
+			<h3>Experience</h3>
+			<div className="exp-1">
+				<h4>Directeur Technique - Google</h4>
+				<h5>2018 - 2020</h5>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+					ut consequuntur tempore ratione sunt? Cumque minima consequuntur
+					explicabo fuga, dolores consectetur fugiat tempora aperiam aliquam
+					maiores provident maxime. Placeat, ullam.
+				</p>
+			</div>
+			<div className="exp-2">
+				<h4>Directeur Technique - Apple</h4>
+				<h5>2018 - 2020</h5>
+				<p>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia laborum
+					molestias eius explicabo dolor omnis consequatur quae ipsa quasi, id
+					perspiciatis saepe! Id voluptate nobis ducimus delectus provident
+					perspiciatis quia.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Experience;
